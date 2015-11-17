b2426fc76370462071744bf2e6ea81b5d45ab394
ansom33 <stu450355@H101-03.wsc.western.edu>
2015-11-17 14:55:35 -0700

    Fact builder/Tester for it
    Fact Builder -- takes token stream and separates pronoun, verb, noun and
    adds to array list of facts

--- AgileNLP/src/TestHarness/TestFacts.java
+++ AgileNLP/src/TestHarness/TestFacts.java
@@ -1,49 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package TestHarness;
 
-/**
- *
- * @author bdoyle
- */
 import Fact.*;
+import FileReader.DataAggregator;
+import FileReader.MovieCharacter;
+import FileReader.MovieComposite;
 import Token.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.Set;
 
 public class TestFacts {
-    public static void main(String[] args) {
-        test1();
-    }    
-    
-    public static void test1()
-    {
-        String path = "c:\\test\\test_data.txt";
-        String file_contents = getContentsofFile(path);
-        TokenStream ts1 = Tokenizer.lexer("What movie is Yoda in?");
-        TokenStream ts2 = Tokenizer.tokenizePlot(file_contents);
-        Facts fs1 = FactBuilder.glean(ts1);
-        Facts fs2 = FactBuilder.glean(ts2);
-        System.out.println("done");
-    }
-    
 
-    public static String getContentsofFile(String path)
-    {
-        
-        try
-        {
-            Charset encoding = Charset.defaultCharset();
-            byte[] encoded = Files.readAllBytes(Paths.get(path));
-            return new String(encoded, encoding);        
-        }
-        catch (Exception ex)
-        {
-            return "";
-        }
+    public static void main(String[] args) {
+        test2();
     }
+
+    public static void found(MovieComposite m) {
+        System.out.println("Found it! " + m.movie.movie_name);
+
+        for (Object o : m.characters) {
+            MovieCharacter c = (MovieCharacter) o;
+            System.out.println("Character: " + c.character_name);
+        }
+
+        System.out.println(m.plot_summary.summary);
+
+    }
+
+    public static void test1() {
+
+        String path = "C:\\Users\\stu450355\\Desktop\\starWarsSynopsis.txt";
+
+        String file_contents = getContentsofFile(path);
+
+// TokenStream ts1 = Tokenizer.lexer("What movie is Yoda in?");
+        TokenStream ts2 = Tokenizer.tokenizePlot(file_contents);
+        ts2 = Tokenizer.tagger(ts2);
+        System.out.println("" + ts2.tokens);
+
+        ArrayList<Token> d = FactBuilder.glean(ts2);
+
+        System.out.println("" + d);
+
+    }
+    
+    public static void test2()
+    {
+        Hashtable composite_movies = DataAggregator.GetCompositeMovies();
+
+        Set<Integer> composite_movie_keys = composite_movies.keySet();  // movie key set  
+
+        composite_movie_keys.stream().forEach((composite_movie_key) -> {
+            MovieComposite cm1 = (MovieComposite)composite_movies.get(composite_movie_key);
+
+            String plot_summary = cm1.plot_summary.summary;
+
+            TokenStream ts2 = Tokenizer.tokenizePlot(plot_summary);
+            ts2 = Tokenizer.tagger(ts2);
+            System.out.println("" + ts2.tokens);
+
+            ArrayList<Token> d = FactBuilder.glean(ts2);
+
+            System.out.println("" + d);
+            
+        });
+        
+    }
+
+    public static String getContentsofFile(String path) {
+
+        try {
+
+            Charset encoding = Charset.defaultCharset();
+
+            byte[] encoded = Files.readAllBytes(Paths.get(path));
+
+            return new String(encoded, encoding);
+
+        } catch (Exception ex) {
+
+            return "";
+
+        }
+
+    }
+
 }
